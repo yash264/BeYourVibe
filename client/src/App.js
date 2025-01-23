6a66de336d797967434daf92b4053dcf7f83c20f
yash264 <yp20222068@gmail.com>
2025-01-23 22:59:46 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h3>Welcome to Client Side</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ server/src/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const PORT = require("port")
+const port = 4000;
+
+app.listen(port , () => {
+    console.log(`Server is running at ${port}`);
+}) 
